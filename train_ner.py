762ab522cacee221f23b3e39510d455c36987282
hoangdzung <hoangdzung@github.com>
2021-10-02 11:23:20 +0200

    add train file

---
+++ train_ner.py
@@ -0,0 +1,106 @@
+
+import torch
+from torch.utils.data import DataLoader 
+from transformers import AutoTokenizer, AutoModelForTokenClassification, AdamW
+import numpy as np
+
+import re
+from pathlib import Path
+from tqdm import tqdm 
+from sklearn.model_selection import train_test_split
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--data")
+parser.add_argument("--save_dir")
+args = parser.parse_args()
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+def read_wnut(file_path):
+    file_path = Path(file_path)
+
+    raw_text = file_path.read_text().strip()
+    raw_docs = re.split(r'\n\t?\n', raw_text)
+    token_docs = []
+    tag_docs = []
+    for doc in raw_docs:
+        tokens = []
+        tags = []
+        for line in doc.split('\n'):
+            token, tag = line.split('\t')
+            tokens.append(token)
+            tags.append(tag)
+        token_docs.append(tokens)
+        tag_docs.append(tags)
+
+    return token_docs, tag_docs
+
+texts, tags = read_wnut(args.data)
+
+train_texts, val_texts, train_tags, val_tags = train_test_split(texts, tags, test_size=.2)
+
+unique_tags = set(tag for doc in tags for tag in doc)
+tag2id = {tag: id for id, tag in enumerate(unique_tags)}
+id2tag = {id: tag for tag, id in tag2id.items()}
+
+tokenizer = AutoTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
+train_encodings = tokenizer(train_texts, is_split_into_words=True, return_offsets_mapping=True, padding=True, truncation=True)
+val_encodings = tokenizer(val_texts, is_split_into_words=True, return_offsets_mapping=True, padding=True, truncation=True)
+
+def encode_tags(tags, encodings):
+    labels = [[tag2id[tag] for tag in doc] for doc in tags]
+    encoded_labels = []
+    for doc_labels, doc_offset in zip(labels, encodings.offset_mapping):
+        # create an empty array of -100
+        doc_enc_labels = np.ones(len(doc_offset),dtype=int) * -100
+        arr_offset = np.array(doc_offset)
+
+        # set labels whose first offset position is 0 and the second is not 0
+        doc_enc_labels[(arr_offset[:,0] == 0) & (arr_offset[:,1] != 0)] = doc_labels
+        encoded_labels.append(doc_enc_labels.tolist())
+
+    return encoded_labels
+
+train_labels = encode_tags(train_tags, train_encodings)
+val_labels = encode_tags(val_tags, val_encodings)
+
+class WNUTDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+train_encodings.pop("offset_mapping") # we don't want to pass this to the model
+val_encodings.pop("offset_mapping")
+train_dataset = WNUTDataset(train_encodings, train_labels)
+val_dataset = WNUTDataset(val_encodings, val_labels)
+
+model = AutoModelForTokenClassification.from_pretrained('allenai/scibert_scivocab_uncased', num_labels=len(unique_tags))
+
+model.to(device)
+model.train()
+
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+
+optim = AdamW(model.parameters(), lr=5e-5)
+
+for epoch in range(3):
+    for batch in tqdm(train_loader):
+        optim.zero_grad()
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs[0]
+        loss.backward()
+        optim.step()
+
+model.save_pretrained(args.save_dir)
